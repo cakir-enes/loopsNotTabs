231e204d809d1defe02620adedeb1f37c66a2219
cakir-enes <caki-enes@outlook.com>
2020-01-03 15:14:38 +0300

    refactor styling

--- src/cljs/loops_not_tabs/views.cljs
+++ src/cljs/loops_not_tabs/views.cljs
@@ -29,20 +29,22 @@
 (defn loop-list []
   (let [loops @(rf/subscribe [:loops])
         active-loop @(rf/subscribe [:active-loop])]
-    [:ul.loops
-      (doall (map-indexed 
+    [:div.loops 
+     [:div.header [:h4 "NAME"] [:h4 "PRACTISE"] [:h4 "INTERVAL"]]
+     [:ul
+      
+      (doall (map-indexed
               (fn [i loop]
                 ^{:key i}
-          
+
                 [:li.loop {:on-click (fn []
                                        (println "Trying play loop")
                                        (rf/dispatch [:play-loop (assoc loop :idx i)]))
-                           :class (when (= (:idx active-loop) i) "active")} 
+                           :class (when (= (:idx active-loop) i) "active")}
                  [:h4.main (str "LOOP " i)]
-                 [:h4.shortcut (str "^+" i)]
-                 [:h4.label (str "Practised ") (format-time (:practise loop))]
+                 [:h4.label (format-time (:practise loop))]
                  [:h4.time (format-loop loop)]])
-              loops))]))
+              loops))]]))
 
 (defn main-panel []
   [:div
